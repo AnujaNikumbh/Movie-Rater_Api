989d0a1bd7fbe5656b548a64e35389ef94d828cc
Anuja <anujanikumbh5@GMAIL.COM>
2021-08-31 13:36:16 +0530

    Added the api app

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework import routers
+from django.conf.urls import include
+from .views import MovieViewset,RatingViewset
+
+router =routers.DefaultRouter()
+router.register('movies',MovieViewset)
+router.register('ratings',RatingViewset)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
