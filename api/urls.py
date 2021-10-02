659a7a4f154ff1af97bac5ae56b50a9b35b399bd
Anuja <anujanikumbh5@GMAIL.COM>
2021-10-02 15:15:02 +0530

    registered users urls

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import MovieViewset,RatingViewset
+from .views import MovieViewset,RatingViewset,UserViewSet
 
 router =routers.DefaultRouter()
 router.register('movies',MovieViewset)
 router.register('ratings',RatingViewset)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
