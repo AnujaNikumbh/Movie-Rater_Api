8e17db63c89537bda615d6a27e13ecbdc39a0a4b
Anuja <anujanikumbh5@GMAIL.COM>
2021-10-02 21:58:50 +0530

    created class usereg and added fields

--- User_Auth/models.py
+++ User_Auth/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator, MinLengthValidator
 
 # Create your models here.
+class UserReg(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=20, validators=[MinLengthValidator(6)])
+    httpsconfirm_password = models.CharField(max_length=20, validators=[MinLengthValidator(6)])
