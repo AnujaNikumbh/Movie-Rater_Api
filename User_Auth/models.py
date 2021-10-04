0915e9f2e521856c35aa4560aded96d1894a54c1
Anuja <anujanikumbh5@GMAIL.COM>
2021-10-04 13:22:07 +0530

    modified

--- User_Auth/models.py
+++ User_Auth/models.py
@@ -3,10 +3,20 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator, MinLengthValidator
 
 # Create your models here.
+'''
 class UserReg(AbstractUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=50, unique=True)
-    password = models.CharField(max_length=20, validators=[MinLengthValidator(6)])
-    httpsconfirm_password = models.CharField(max_length=20, validators=[MinLengthValidator(6)])
+   f_name = models.CharField(max_length=50)
+   l_name = models.CharField(max_length=50)
+   username = models.CharField(max_length=50, unique=True)
+   email = models.EmailField(max_length=50, unique=True)
+   password = models.CharField(max_length=20, validators=[MinLengthValidator(6)])
+   c_password = models.CharField(max_length=20, validators=[MinLengthValidator(6)])
+
+   REQUIRED_FIELDS = []
+
+   def __str__(self):
+      return 'UserName: ' + self.username
+
+   #USERNAME_FIELD = 'email'
+   #REQUIRED_FIELDS = [] 
+'''
