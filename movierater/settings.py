0c210bfdc1ef001e3a280e11da8c1c41a7606ec0
Anuja <anujanikumbh5@GMAIL.COM>
2021-10-06 22:39:58 +0530

    added rest framework and corsheaders setting

--- movierater/settings.py
+++ movierater/settings.py
@@ -89,7 +89,8 @@ DATABASES = {
 # Rest Framework settings
 '''REST_FRAMEWORK = {
        'DEFAULT_AUTHENTICATION_CLASSES': (
-               'rest_framework.authentication.TokenAuthentication',
+               #'rest_framework.authentication.TokenAuthentication',
+               'User_Auth.verify.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES' : {
         'rest_framework.permissions.IsAuthenticated',
@@ -146,3 +147,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #AUTH_USER_MODEL = 'User_Auth.User'
 CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_CREDENTIALS= True
+#CORS_ORIGIN_WHITELIST=[' http://127.0.0.1:8000/']
