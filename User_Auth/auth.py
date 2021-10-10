e51e9dffe2a8c5fe584abfcbfbda68cccf5063f3
Anuja <anujanikumbh5@GMAIL.COM>
2021-10-10 19:47:55 +0530

    defined access token and refresh token

--- User_Auth/auth.py
+++ User_Auth/auth.py
@@ -1,12 +1,12 @@
 import datetime
 import jwt
 from django.conf import settings
- 
+
 def generate_access_token(user):
-    
+
     access_token_payload = {
         'user_id': user.id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=5),
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=60),
         'iat': datetime.datetime.utcnow(),
     }
     access_token = jwt.encode(access_token_payload,
