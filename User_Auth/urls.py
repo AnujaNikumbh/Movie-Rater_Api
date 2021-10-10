51fec92f923b9f8b139cb2f76bd04d30665f6979
Anuja <anujanikumbh5@GMAIL.COM>
2021-10-10 19:47:17 +0530

    added logout url

--- User_Auth/urls.py
+++ User_Auth/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView
+from .views import RegisterView, login_view,User_logout,refresh_token_view,user
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view()),
+    #path('login', LoginView.as_view()),
+    #path('user', UserView.as_view()),
+    path('user',user,name='user'),
+    path('logout', User_logout,name='logout'),
+    path('login',login_view,name='login'),
+    path('refresh_token',refresh_token_view),
+    #path('logout', LogoutView.as_view()),
 
 ]
